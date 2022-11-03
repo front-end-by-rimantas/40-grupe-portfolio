a2bc499d118ec5efeadcd134d4a68abea95918f5
Rimantas <belauzas@yahoo.com>
2022-11-03 11:55:50 +0200

    Git conflict prevention

---
+++ js/pages/home.js
@@ -0,0 +1,42 @@
+// IMPORTS
+
+// EXECUTION
+
+/* HEADER-start */
+/* HEADER-end */
+
+/* HERO-start */
+/* HERO-end */
+
+/* ABOUT-ME-start */
+/* ABOUT-ME-end */
+
+/* HOBBIES-start */
+/* HOBBIES-end */
+
+/* ACHIEVEMENTS-start */
+/* ACHIEVEMENTS-end */
+
+/* SERVICES-start */
+/* SERVICES-end */
+
+/* WORK-start */
+/* WORK-end */
+
+/* EXPERTISE-start */
+/* EXPERTISE-end */
+
+/* PORTFOLIO-start */
+/* PORTFOLIO-end */
+
+/* TESTIMONIALS-start */
+/* TESTIMONIALS-end */
+
+/* HIRE-ME-start */
+/* HIRE-ME-end */
+
+/* CONTACTS-start */
+/* CONTACTS-end */
+
+/* FOOTER-start */
+/* FOOTER-end */
